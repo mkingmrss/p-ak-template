1ef33cab667378b81023878ec0c1d1cb2f37a431
Michael King <mking@mrss.com>
2017-06-15 13:54:04 -0400

    start building out css

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+var gulp        = require( 'gulp' );
+var sass        = require( 'gulp-sass' );
+var watch       = require( 'gulp-watch' );
+
+/*var processHtml = require( 'gulp-processhtml' );
+var rename      = require( 'gulp-rename' );
+var uglify      = require( 'gulp-uglify' );
+var concat      = require( 'gulp-concat' );
+var insert      = require( 'gulp-insert' );*/
+ 
+
+//
+//  SCSS
+//
+gulp.task( 'sass', function() {
+  console.log( 'sassing...' );
+  gulp.src( './src/sass/main.scss' )
+    .pipe( sass() )
+    .pipe( gulp.dest( './localhost/css/' ) );
+});
+
+//
+//  Watch
+//
+gulp.task( 'watch', function() {
+  gulp.watch( './src/sass/*.scss', ['sass'] );
+  gulp.watch( './src/sass/*.css', ['sass'] );
+});
+
+//
+//  Build tasks
+//
+gulp.task( 'default', [ 'sass' ] );
